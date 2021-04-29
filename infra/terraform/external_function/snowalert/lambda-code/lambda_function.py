36cbfa6a3dcc8b76e6d573aed329a5774fea8be3
sfc-gh-pkommini <prasanth.kommini@snowflake.com>
2021-04-29 11:06:44 +0530

    Add async

--- infra/terraform/external_function/snowalert/lambda-code/lambda_function.py
+++ infra/terraform/external_function/snowalert/lambda-code/lambda_function.py
@@ -1,16 +1,24 @@
+import json
+import os.path
 import re
+import sys
 from codecs import encode
 from importlib import import_module
 from json import dumps, loads
-import re
-import sys
-import os.path
+from typing import Any, Dict, Optional, Text
+from urllib.parse import urlparse
 
+import boto3
+
+from drivers import destination_s3
 from vault import decrypt_if_encrypted
 
 # pip install --target ./site-packages -r requirements.txt
 dir_path = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(dir_path, 'site-packages'))
+
+HTTP_METHOD_STRING = 'httpMethod'
+REGION_NAME = 'us-west-2'
 
 
 def zip(s, chunk_size=1_000_000):
@@ -42,7 +50,107 @@ def format(s, ps):
     return ps[int(m.group(1))] if m else replace_refs(s, ps)
 
 
-def lambda_handler(event, context=None):
+def create_response(code, msg):
+    return {'statusCode': code, 'body': msg}
+
+
+def invoke_process_lambda(batch_id, data, lambda_name):
+    # Create payload to be sent to lambda
+    invoke_payload = json.dumps(data)
+
+    # Invoke processing lambda asynchronously by using InvocationType='Event'.
+    # This allows the processing to continue while the POST handler returns HTTP 202.
+    lambda_client = boto3.client(
+        'lambda',
+        region_name=REGION_NAME,
+    )
+    lambda_response = lambda_client.invoke(
+        FunctionName=lambda_name, InvocationType='Event', Payload=invoke_payload
+    )
+    # returns 202 on success if InvocationType = 'Event'
+    return lambda_response
+
+
+def async_flow_poll(event: Any, destination: str, context: Any) -> Dict[str, Any]:
+    """
+    Repeatedly checks on the status of the batch_ID, and returns the result after the
+    processing has been completed
+
+    Args:
+        event (Any):
+        destination (str):
+        context (Any):
+
+    Returns:
+        Dict[str, Any]:
+    """
+    batch_id = event['headers']['sf-external-function-query-batch-id']
+    processed_data = read_data_from_storage(batch_id)
+    # print(f'data from storage in poll {processed_data}')
+    def parse_processed_data(response):
+        # in this case, the response is the response from DynamoDB
+        response_metadata = response['ResponseMetadata']
+        status_code = response_metadata['HTTPStatusCode']
+
+        # Take action depending on item status
+        item = response['Item']
+        job_status = item['status']
+        if job_status == SUCCESS_STATUS:
+
+            file = item['data']
+            s3 = boto3.client('s3')
+
+            obj = s3.get_object(Bucket=IT_DATA_INGEST_BUCKET, Key=file)
+            data = eval(obj['Body'].read().decode('utf-8'))
+
+            return {'statusCode': 200, 'body': json.dumps({'data': data})}
+        elif job_status == IN_PROGRESS_STATUS:
+            return {'statusCode': 202, "body": "{}"}
+        else:
+            return create_response(500, "Error in poll: Unknown item status.")
+
+    return parse_processed_data(processed_data)
+
+
+def async_flow_init(event, context):
+    """
+    - Reads batch_ID and data from the request,
+    - marks the batch_ID as being processed, and
+    - starts the processing service.
+    """
+    batch_id = event['headers']['sf-external-function-query-batch-id']
+    destination = event['headers']['sf-custom-destination']
+
+    lambda_name = context.function_name
+    write_driver = import_module(f'drivers.write_{urlparse(destination).scheme}')
+    write_driver.init(destination, batch_id)
+    lambda_response = invoke_process_lambda(batch_id, event, lambda_name)
+
+    # lambda response returns 202, because we are invoking it with
+    # InvocationType = 'Event'
+    if lambda_response["StatusCode"] != 202:
+        response = create_response(
+            400, "Error in inititate: processing lambda not started"
+        )
+    else:
+        response = {'statusCode': lambda_response["StatusCode"]}
+
+    return response
+
+
+def lambda_handler(event, context):
+    destination = event['headers'].get('sf-custom-destination')
+    method = event.get('httpMethod', 'GET')
+
+    if destination:  # This requires async flow
+        return async_flow_init(event, context)
+    elif method == 'GET':  # This requires polling the write destination
+        return async_flow_poll(event, context)
+    elif method == 'POST':  # This means it's request from API gateway
+        return sync_flow(event, context)
+
+
+def sync_flow(event, context=None):
     headers = event['headers']
     response_encoding = headers.pop('sf-custom-response-encoding', None)
 
@@ -57,7 +165,6 @@ def lambda_handler(event, context=None):
         }
 
         try:
-
             driver, *path = event['path'].lstrip('/').split('/')
             driver = driver.replace('-', '_')
             process_row = import_module(f'drivers.process_{driver}').process_row
@@ -72,9 +179,7 @@ def lambda_handler(event, context=None):
                 zip(dumps(row_result)) if response_encoding == 'gzip' else row_result,
             ]
         )
-
     data_dumps = dumps({'data': res_data})
-
     if len(data_dumps) > 6_000_000:
         data_dumps = dumps(
             {
--- infra/terraform/external_function/snowalert/lambda-code/utils.py
+++ infra/terraform/external_function/snowalert/lambda-code/utils.py
@@ -1,6 +1,9 @@
-from typing import Optional, Any
-
+import json
 import re
+import time
+from typing import Any, Dict, Optional, Text
+
+import boto3
 
 
 def pick(path: str, d: dict):
@@ -20,9 +23,7 @@ def parse_header_links(value):
 
     :rtype: list
     """
-
     links = []
-
     replace_chars = ' \'"'
 
     value = value.strip(replace_chars)
@@ -48,3 +49,96 @@ def parse_header_links(value):
         links.append(link)
 
     return links
+
+
+def initiate(event: Dict, context):
+    """
+    Reads batch_ID and data from the request, marks the batch_ID as being processed, and
+    starts the processing service.
+
+    Args:
+        event ([type]): [description]
+        context ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """
+    batch_id = event[HEADERS_STRING][BATCH_ID_STRING]
+    data = event
+
+    lambda_name = context.function_name
+
+    write_to_storage(batch_id, IN_PROGRESS_STATUS, "NULL")
+    lambda_response = invoke_process_lambda(batch_id, data, lambda_name)
+
+    # lambda response returns 202, because we are invoking it with
+    # InvocationType = 'Event'
+    if lambda_response["StatusCode"] != 202:
+        response = create_response(
+            400, "Error in inititate: processing lambda not started"
+        )
+    else:
+        response = {'statusCode': lambda_response["StatusCode"]}
+    return response
+
+
+def create_response(code, msg):
+    return {'statusCode': code, 'body': msg}
+
+
+def invoke_process_lambda(batch_id, data, lambda_name):
+    # Create payload to be sent to processing lambda
+    invoke_payload = json.dumps(data).replace(
+        '"sf-custom-call-type": "async"', '"sf-custom-call-type": "sync-parent"'
+    )
+
+    # Invoke processing lambda asynchronously by using InvocationType='Event'.
+    # This allows the processing to continue while the POST handler returns HTTP 202.
+    lambda_client = boto3.client(
+        'lambda',
+        region_name=REGION_NAME,
+    )
+    print('before async')
+
+    print(f'invoke payload: {invoke_payload}')
+    lambda_response = lambda_client.invoke(
+        FunctionName=lambda_name, InvocationType='Event', Payload=invoke_payload
+    )
+    print(f'after async in')
+    # returns 202 on success if InvocationType = 'Event'
+    return lambda_response
+
+
+def write_to_storage(batch_id, status, data):
+    # we assume that the table has already been created
+    client = boto3.resource('dynamodb')
+    table = client.Table(TABLE_NAME)
+
+    s3_client = boto3.client('s3')
+    file = 'async_response_' + batch_id + '.json'
+    key = 'it/async_system_response/' + file
+    response = s3_client.put_object(
+        Bucket=IT_DATA_INGEST_BUCKET, Body=str(data), Key=key
+    )
+
+    item_ts = int(time.time())
+    item_expire_ts = int(time.time() + 60 * 60)
+
+    # Put in progress item in table
+    item_to_store = {
+        'batch_id': batch_id,
+        'status': status,
+        'data': key,
+        'timestamp': item_ts,
+        'ttl': item_expire_ts,
+    }
+    db_response = table.put_item(Item=item_to_store)
+
+
+def read_data_from_storage(batch_id):
+    # we assume that the table has already been created
+    client = boto3.resource('dynamodb')
+    table = client.Table(TABLE_NAME)
+
+    response = table.get_item(Key={'batch_id': batch_id}, ConsistentRead=True)
+    return response
